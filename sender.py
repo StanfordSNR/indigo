798c5f293d28d4934c4044833284da96c2cd51a6
Francis Y. Yan <fyy@cs.stanford.edu>
2017-03-18 12:55:10 -0700

    Sender takes state_dim, action_num, max_steps as args

--- sender.py
+++ sender.py
@@ -9,23 +9,22 @@ from helpers import curr_ts_ms
 
 
 class Sender(object):
-    def __init__(self, ip, port, sample_action):
+    def __init__(self,
+                 ip,
+                 port,
+                 state_dim,
+                 action_num,
+                 sample_action,  # sample_action(state) is a function
+                 max_steps):
         self.dest_addr = (ip, port)
-        # sample_action(state) is a function
+        self.state_dim = state_dim
+        self.action_num = action_num
         self.sample_action = sample_action
-
-        self.state_dim = 10
-        self.action_num = 3
+        self.max_steps = max_steps
 
         self.rtts = []
         self.state_buf = []
         self.action_buf = []
-
-    def get_state_dim(self):
-        return self.state_dim
-
-    def get_action_num(self):
-        return self.action_num
 
     def get_curr_state(self):
         if len(self.rtts) < self.state_dim:
@@ -36,10 +35,12 @@ class Sender(object):
     def loop(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s = self.s
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         data = {}
         data['payload'] = 'x' * 1400
-        while True:
+
+        for t in xrange(self.max_steps):
             state = self.get_curr_state()
             self.state_buf.append(state)
 
@@ -56,6 +57,9 @@ class Sender(object):
             rtt = curr_ts_ms() - ack['send_ts']
             self.rtts.append(rtt)
 
+    def get_experience(self):
+        return self.state_buf, self.action_buf
+
     def cleanup(self):
         self.s.close()
 
@@ -70,7 +74,7 @@ def main():
         time.sleep(1)
         return 1
 
-    sender = Sender(args.ip, args.port, test_sample_action)
+    sender = Sender(args.ip, args.port, 10, 3, test_sample_action, 10)
     try:
         sender.loop()
     except:
