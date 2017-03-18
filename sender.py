8682b5988c185922777a81657af5ba54a6be3e9a
Francis Y. Yan <fyy@cs.stanford.edu>
2017-03-18 00:32:04 -0700

    sender.py: takes sample_action(state) as an argument

--- sender.py
+++ sender.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python
 
 import sys
+import time
 import json
 import socket
-import signal
 import argparse
 from helpers import curr_ts_ms
 
 
-EPISODE_TIME = 60  # time (s) of each episode of experience
-
-
 class Sender(object):
-    def __init__(self, dest_addr):
+    def __init__(self, ip, port, sample_action):
+        self.dest_addr = (ip, port)
+        # sample_action(state) is a function
+        self.sample_action = sample_action
+
+        self.state_dim = 10
+        self.action_num = 3
+
+        self.rtts = []
+        self.state_buf = []
+        self.action_buf = []
+
+    def get_state_dim(self):
+        return self.state_dim
+
+    def get_action_num(self):
+        return self.action_num
+
+    def get_curr_state(self):
+        if len(self.rtts) < self.state_dim:
+            return [0] * (self.state_dim - len(self.rtts)) + self.rtts
+        else:
+            return self.rtts[-self.state_dim:]
+
+    def loop(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.dest_addr = dest_addr
+        s = self.s
 
-    # dummy version
-    def get_next_action(self):
-        return 1
-
-    def start(self):
         data = {}
         data['payload'] = 'x' * 1400
-
         while True:
-            # determine the next action
-            send_cnt = self.get_next_action()
-            for i in xrange(send_cnt):
-                data['send_ts'] = curr_ts_ms()
-                self.s.sendto(json.dumps(data), self.dest_addr)
+            state = self.get_curr_state()
+            self.state_buf.append(state)
 
-            # wait for a single ACK
-            raw_ack = self.s.recvfrom(1500)[0]
+            action = self.sample_action(state)
+            self.action_buf.append(action)
+
+            # send #action datagrams
+            for i in xrange(action):
+                data['send_ts'] = curr_ts_ms()
+                s.sendto(json.dumps(data), self.dest_addr)
+
+            raw_ack = s.recvfrom(1500)[0]
             ack = json.loads(raw_ack)
             rtt = curr_ts_ms() - ack['send_ts']
+            self.rtts.append(rtt)
 
-            # TODO: store RTTs as states
-
-
-    def stop(self):
+    def cleanup(self):
         self.s.close()
-
-
-def timeout_handler(signum, frame):
-    raise
 
 
 def main():
@@ -53,18 +66,17 @@ def main():
     parser.add_argument('port', type=int)
     args = parser.parse_args()
 
-    dest_addr = (args.ip, args.port)
-    sender = Sender(dest_addr)
+    def test_sample_action(state):
+        time.sleep(1)
+        return 1
 
-    signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(EPISODE_TIME)
-
+    sender = Sender(args.ip, args.port, test_sample_action)
     try:
-        sender.start()
+        sender.loop()
     except:
-        sys.stderr.write('Finished one episode of experience\n')
-
-    sender.stop()
+        pass
+    finally:
+        sender.cleanup()
 
 
 if __name__ == '__main__':
