1b7b80945bd23dc3d57087d66e611527c8329766
Francis Y. Yan <fyy@cs.stanford.edu>
2017-03-18 15:51:55 -0700

    helpers.py: add a test initializer for RL params used by Sender

--- helpers.py
+++ helpers.py
@@ -3,3 +3,19 @@ import time
 
 def curr_ts_ms():
     return int(time.time() * 1000)
+
+
+def test_sender_rl_params():
+    def test_sample_action(state):
+        time.sleep(1)
+        return 1
+
+    rl_params = {
+        'state_dim': 10,
+        'action_num': 3,
+        'max_steps': 10,
+        'delay_weight': 0.8,
+        'sample_action': test_sample_action,
+    }
+
+    return rl_params
