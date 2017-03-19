6664fcb27e9f2ac83a3a623b8b763e9245637dfd
Francis Y. Yan <fyy@cs.stanford.edu>
2017-03-18 17:11:11 -0700

    helpers.py: add time_filename()

--- helpers.py
+++ helpers.py
@@ -5,6 +5,10 @@ def curr_ts_ms():
     return int(time.time() * 1000)
 
 
+def time_filename():
+    return time.strftime('%Y%m%d-%H%M%S')
+
+
 def test_sender_rl_params():
     def test_sample_action(state):
         time.sleep(1)
