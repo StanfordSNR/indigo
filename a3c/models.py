625f039394b0fd29d19d033841c285f9b2af8742
Francis Y. Yan <fyy@cs.stanford.edu>
2017-05-02 21:16:37 -0700

    Add additional hidden layers to ActorCriticNetwork

--- a3c/models.py
+++ a3c/models.py
@@ -1,17 +1,24 @@
 import tensorflow as tf
-from tensorflow.contrib import layers
+from tensorflow.contrib import layers, rnn
 
 
 class ActorCriticNetwork(object):
     def __init__(self, state_dim, action_cnt):
         self.states = tf.placeholder(tf.float32, [None, state_dim])
 
-        actor_h1 = layers.relu(self.states, 10)
-        self.action_scores = layers.linear(actor_h1, action_cnt)
+        actor_h1 = layers.relu(self.states, 8)
+        actor_h2 = layers.relu(actor_h1, 8)
+        self.action_scores = layers.linear(actor_h2, action_cnt)
         self.action_probs = tf.nn.softmax(self.action_scores)
 
-        critic_h1 = layers.relu(self.states, 10)
-        self.state_values = tf.reshape(layers.linear(critic_h1, 1), [-1])
+        critic_h1 = layers.relu(self.states, 8)
+        critic_h2 = layers.relu(critic_h1, 8)
+        self.state_values = tf.reshape(layers.linear(critic_h2, 1), [-1])
 
         self.trainable_vars = tf.get_collection(
             tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+
+
+class ActorCriticLSTM(object):
+    def __init__(self, state_dim, action_cnt):
+        pass
