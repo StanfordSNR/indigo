b1a660eb78e2d109fd1728b1e191c9ba1d49e217
Francis Y. Yan <fyy@cs.stanford.edu>
2017-09-10 23:21:42 -0700

    Add models.py and run_sender.py

---
+++ rl/models.py
@@ -0,0 +1,59 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import layers, rnn
+
+
+class RLLSTM(object):
+    def __init__(self, state_dim, action_cnt):
+        # self.input: [batch_size, max_time, state_dim]
+        self.input = tf.placeholder(tf.float32, [None, None, state_dim])
+
+        self.num_layers = 2
+        self.lstm_dim = 64
+        stacked_lstm = rnn.MultiRNNCell([rnn.BasicLSTMCell(self.lstm_dim)
+            for _ in xrange(self.num_layers)])
+
+        self.state_in = []
+        state_tuple_in = []
+        for _ in xrange(self.num_layers):
+            c_in = tf.placeholder(tf.float32, [None, self.lstm_dim])
+            h_in = tf.placeholder(tf.float32, [None, self.lstm_dim])
+            self.state_in.append((c_in, h_in))
+            state_tuple_in.append(rnn.LSTMStateTuple(c_in, h_in))
+
+        self.state_in = tuple(self.state_in)
+        state_tuple_in = tuple(state_tuple_in)
+
+        # self.output: [batch_size, max_time, lstm_dim]
+        output, state_tuple_out = tf.nn.dynamic_rnn(
+            stacked_lstm, self.input, initial_state=state_tuple_in)
+
+        self.state_out = self.convert_state_out(state_tuple_out)
+
+        # map output to scores
+        self.action_scores = layers.linear(output, action_cnt)
+        self.action_probs = tf.nn.softmax(self.action_scores)
+
+        self.dagger_trainable_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+
+        self.state_values = layers.linear(output, 1)
+
+        self.all_trainable_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+
+    def convert_state_out(self, state_tuple_out):
+        state_out = []
+        for lstm_state_tuple in state_tuple_out:
+            state_out.append((lstm_state_tuple.c, lstm_state_tuple.h))
+
+        return tuple(state_out)
+
+    def zero_init_state(self, batch_size):
+        init_state = []
+        for _ in xrange(self.num_layers):
+            c_init = np.zeros([batch_size, self.lstm_dim], np.float32)
+            h_init = np.zeros([batch_size, self.lstm_dim], np.float32)
+            init_state.append((c_init, h_init))
+
+        return init_state
---
+++ rl/run_sender.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import argparse
+import project_root
+import numpy as np
+import tensorflow as tf
+from os import path
+from env.sender import Sender
+from models import RLLSTM
+from helpers.helpers import normalize, softmax
+
+
+class Learner(object):
+    def __init__(self, state_dim, action_cnt, restore_vars):
+        with tf.variable_scope('global'):
+            self.model = RLLSTM(state_dim=state_dim, action_cnt=action_cnt)
+
+        self.lstm_state = self.model.zero_init_state(1)
+
+        self.sess = tf.Session()
+
+        # restore saved variables
+        saver = tf.train.Saver(self.model.dagger_trainable_vars)
+        saver.restore(self.sess, restore_vars)
+
+        # init the remaining vars, especially those created by optimizer
+        uninit_vars = set(tf.global_variables())
+        uninit_vars -= set(self.model.dagger_trainable_vars)
+        self.sess.run(tf.variables_initializer(uninit_vars))
+
+    def sample_action(self, state):
+        state = normalize(state)
+
+        # Get probability of each action from the local network.
+        pi = self.model
+        feed_dict = {
+            pi.input: [[state]],
+            pi.state_in: self.lstm_state,
+        }
+        ops_to_run = [pi.action_probs, pi.state_out]
+        action_probs, self.lstm_state = self.sess.run(ops_to_run, feed_dict)
+
+        # Choose an action to take
+        action = np.argmax(action_probs[0][0])
+
+        # action = np.argmax(np.random.multinomial(1, action_probs[0] - 1e-5))
+        # temperature = 1.0
+        # temp_probs = softmax(action_probs[0] / temperature)
+        # action = np.argmax(np.random.multinomial(1, temp_probs - 1e-5))
+        return action
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int)
+    args = parser.parse_args()
+
+    sender = Sender(args.port)
+
+    model_path = path.join(project_root.DIR, 'dagger', 'model', 'model')
+
+    learner = Learner(
+        state_dim=Sender.state_dim,
+        action_cnt=Sender.action_cnt,
+        restore_vars=model_path)
+
+    sender.set_sample_action(learner.sample_action)
+
+    try:
+        sender.handshake()
+        sender.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sender.cleanup()
+
+
+if __name__ == '__main__':
+    main()
