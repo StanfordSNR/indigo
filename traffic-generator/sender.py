d0a9955581ffbf5f063f63e97dc34b89b31f53d3
Francis Y. Yan <fyy@cs.stanford.edu>
2018-10-27 19:12:24 -0700

    traffic-generator/sender.py: clean up and sanity checks

--- traffic-generator/sender.py
+++ traffic-generator/sender.py
@@ -16,45 +16,56 @@
 
 
 import os
+import sys
 import socket
 import subprocess
-import sys
 import time
-import argparse
 import ast
 import bisect
+import argparse
 
 
 class Function(object):
-    def __init__(self, points, step_s=0.01):
-        # validate points
+    def __init__(self, points, bin_s=0.01, max_s=60.0):
+        # sort and validate points
         points = sorted(points, key=lambda tup: tup[0])
-        assert(points[0][0] == 0)
-        assert(points[-1][0] > 0)
+        self.validate_points(points)
+        self.points = points
+
+        # the last point defines the period of the function
         self.period = points[-1][0]
 
-        x = []
-        y = []
-        for point in points:
-            x.append(point[0])
-            y.append(point[1])
+        # unzip points
+        x, y = zip(*points)
 
-        self.step_s = step_s
+        # function's value in each bin
+        self.bin_s = bin_s
+        self.max_s = max_s
+        num_bins = int(max_s / bin_s)
         self.value_bins = []
 
-        # calculate value_bins
-        lower_s = 0.0
-        upper_s = 60.0
+        for bin_idx in xrange(num_bins):
+            r = (bin_idx * bin_s) % self.period
+            i = bisect.bisect(x, r)
+            v_mbps = (y[i] - y[i-1]) * (r - x[i-1]) / (x[i] - x[i-1]) + y[i-1]
 
-        curr_s = lower_s
-        while curr_s < upper_s:
-            residue = curr_s % self.period
-            i = bisect.bisect(x, residue)
-            f = (y[i] - y[i-1]) * (residue - x[i-1]) / (x[i] - x[i-1]) + y[i-1]
+            v_bps = int(v_mbps * 1000 * 1000 / 8) * 8  # align to byte boundary
+            self.value_bins.append(v_bps)
 
-            value_bps = int(f * 1000 * 1000 / 8) * 8
-            self.value_bins.append(value_bps)
-            curr_s += self.step_s
+    def validate_points(self, points):
+        if points[0][0] != 0:
+            sys.exit('The first point must define at x=0')
+
+        if len(points) < 2:
+            sys.exit('The number of points defining a function must be >= 2')
+
+        for i in xrange(len(points)):
+            if points[i][1] < 0:
+                sys.exit('Invalid points: negative y-coordinate')
+
+            if i > 0 and points[i][0] == points[i - 1][0]:
+                sys.exit('Invalid points: same x-coordinate')
+
 
 
 def generate_traffic(addr, port, dev, points):
@@ -68,7 +79,7 @@ def generate_traffic(addr, port, dev, points):
     subprocess.Popen('tc qdisc add dev {} root fq maxrate {}'.format(
                      dev, func.value_bins[0]), shell=True)
     cmd_time = time.time() - pre_cmd_time
-    sleep_time = func.step_s - cmd_time
+    sleep_time = func.bin_s - cmd_time
 
     now = time.time()
     try:
