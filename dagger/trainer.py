8c625c829b921b9c0399f16ce96d7d0c42e0819a
Francis Y. Yan <fyy@cs.stanford.edu>
2018-10-31 16:58:29 -0700

    Clean up trainer.py

--- dagger/trainer.py
+++ dagger/trainer.py
@@ -16,14 +16,9 @@
 #     limitations under the License.
 
 
-import os
 import sys
-import yaml
 import argparse
-import threading
 import tensorflow as tf
-from os import path
-from subprocess import check_call
 
 import context
 from dagger_class import DaggerLeader, DaggerWorker
@@ -31,48 +26,60 @@ from env.mininet_env import MininetEnv
 from helpers.utils import Config
 
 
-def create_mininet_env(worker_num, worker_index):
-    # get total env and tp set
+def create_mininet_env(worker_cnt, worker_index):
+    # settings of emulated networks (env) and background traffic patterns (tp)
     total_env_set = Config.total_env_set_train
     total_tp_set = Config.total_tp_set_train
     total_env_len = len(total_env_set)
-    tasks_per_work = total_env_len / worker_num
+    tasks_per_worker = total_env_len / worker_cnt
 
-    # allocate the env and tp for this worker
+    # allocate env and tp to this worker
     env_set = []
     tp_set = []
-    if worker_index < worker_num - 1:
-        for i in xrange(tasks_per_work * worker_index,
-                        tasks_per_work * (worker_index + 1)):
-            tp_set.append(total_tp_set[i])
-            env_set.append(total_env_set[i])
-            sys.stderr.write('worker {} is allocated tp & env: {} {}\n'.format(
-                worker_index, total_tp_set[i], total_env_set[i]))
-    else:  # last one
-        for i in xrange(tasks_per_work * worker_index, total_env_len):
+
+    if worker_index < worker_cnt - 1:
+        for i in xrange(tasks_per_worker * worker_index,
+                        tasks_per_worker * (worker_index + 1)):
             tp_set.append(total_tp_set[i])
             env_set.append(total_env_set[i])
 
-            sys.stderr.write('worker {} is allocated tp & env: {} {}\n'.format(
-                worker_index, total_tp_set[i], total_env_set[i]))
+            sys.stderr.write('worker {} is allocated with tp {} and env {}\n'
+                .format(worker_index, total_tp_set[i], total_env_set[i]))
+    else:  # last worker
+        for i in xrange(tasks_per_worker * worker_index, total_env_len):
+            tp_set.append(total_tp_set[i])
+            env_set.append(total_env_set[i])
 
-    env = EnvironmentMininet(tp_set, env_set, True)
+            sys.stderr.write('worker {} is allocated with tp {} and env {}\n'
+                .format(worker_index, total_tp_set[i], total_env_set[i]))
+
+    env = MininetEnv(tp_set, env_set, True)
     return env
 
 
-def run(args):
-    """ For each worker/parameter server, start the appropriate job
-    associated with the cluster and server.
-    """
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--ps-hosts', metavar='[HOSTNAME:PORT, ...]', required=True,
+        help='comma-separated list of hostname:port of parameter servers')
+    parser.add_argument(
+        '--worker-hosts', metavar='[HOSTNAME:PORT, ...]', required=True,
+        help='comma-separated list of hostname:port of workers')
+    parser.add_argument('--job-name', choices=['ps', 'worker'],
+                        required=True, help='ps or worker')
+    parser.add_argument('--task-index', metavar='N', type=int, required=True,
+                        help='index of task')
+    args = parser.parse_args()
 
     job_name = args.job_name
     task_index = args.task_index
-    sys.stderr.write('Starting job %s task %d\n' % (job_name, task_index))
+    sys.stderr.write('Starting job {} task {}\n'.format(job_name, task_index))
 
     ps_hosts = args.ps_hosts.split(',')
     worker_hosts = args.worker_hosts.split(',')
     num_workers = len(worker_hosts)
 
+    # start the appropriate job associated with tensorflow cluster and server
     cluster = tf.train.ClusterSpec({'ps': ps_hosts, 'worker': worker_hosts})
     server = tf.train.Server(cluster, job_name=job_name, task_index=task_index)
 
@@ -97,24 +104,6 @@ def run(args):
             pass
         finally:
             worker.cleanup()
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--ps-hosts', required=True, metavar='[HOSTNAME:PORT, ...]',
-        help='comma-separated list of hostname:port of parameter servers')
-    parser.add_argument(
-        '--worker-hosts', required=True, metavar='[HOSTNAME:PORT, ...]',
-        help='comma-separated list of hostname:port of workers')
-    parser.add_argument('--job-name', choices=['ps', 'worker'],
-                        required=True, help='ps or worker')
-    parser.add_argument('--task-index', metavar='N', type=int, required=True,
-                        help='index of task')
-    args = parser.parse_args()
-
-    # run parameter servers and workers
-    run(args)
 
 
 if __name__ == '__main__':
